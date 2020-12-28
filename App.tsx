d42677c05dfdfba33c6faee303dde729620d3170
Ian Huet <ian@huet.info>
2020-12-28 20:41:34 +0000

    add heading & compass. remove geocoding.

--- App.tsx
+++ App.tsx
@@ -9,94 +9,84 @@ interface ILocation {
   longitude: number;
 }
 
-interface IGeocode {
-  city: string|null;
-  district: string|null;
-  street: string|null;
-  region: string|null;
-  subregion: string|null;
-  postalCode: string|null;
-  country: string| null;
-  name: string|null;
-  isoCountryCode: string|null;
-  timezone: string|null;
-}
-
 export default function App() {
+  const [permissionGranted, setPermissionGranted] = useState<boolean>(false)
+  const [heading, setHeading] = useState<number>(0)
   const [location, setLocation] = useState<ILocation>({
     latitude: 0,
     longitude: 0,
   })
-  const [geocode, setGeocode] = useState<IGeocode>({
-    city: null,
-    district: null,
-    street: null,
-    region: null,
-    subregion: null,
-    postalCode: null,
-    country:  null,
-    name: null,
-    isoCountryCode: null,
-    timezone: null,
-  })
   const [errorMessage, setErrorMessage] = useState<string>('')
 
-  useEffect(() => {
-    getLocationAsync()
-  }, [])
+  const getPermissions = async () => {
+    let permissions = await Permissions.getAsync(Permissions.LOCATION)
 
-  const getGeocodeAsync = async (location: ILocation) => {
+    if (permissions.granted) {
+      setPermissionGranted(true)
+    } else {
+      permissions = await Permissions.askAsync(Permissions.LOCATION)
 
-    console.log(`Location: ${JSON.stringify(location)}`)
-
-    const googleApiKey = 'AIzaSyD1KHCXa1E8lXFdZshXL-4_I4iVCb3BYgo'
-    Location.setGoogleApiKey(googleApiKey)
-    let reverseGeocode:IGeocode|null = await Location.reverseGeocodeAsync(location)
-
-    console.log(`Geocode: ${JSON.stringify(reverseGeocode)}`)
-
-    setGeocode(reverseGeocode)
+      if (permissions.granted) {
+        setPermissionGranted(true)
+      } else {
+        setErrorMessage('Permission to access location was denied')
+      }
+    }
   }
 
-  const getLocationAsync = async () => {
-    let permissions = await Permissions.getAsync(Permissions.LOCATION);
-    if (!permissions.granted) {
-      permissions = await Permissions.askAsync(Permissions.LOCATION)
-    }
-
-    if (!permissions.granted) {
-      setErrorMessage('Permission to access location was denied')
-    }
-
-    const currentLocation = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest })
+  const watchPosition = async () => {
+    // const currentLocation = await Location.getCurrentPositionAsync({ accuracy: Location.Accuracy.Highest })
     // const currentLocation: object | null = await Location.getLastKnownPositionAsync()
 
-    if (currentLocation !== null) {
-      console.log(`GLA Location: ${JSON.stringify(currentLocation)}`)
-
-      const { latitude, longitude } = currentLocation.coords
-
-      getGeocodeAsync({ latitude, longitude }).catch(error => setErrorMessage(error))
+    Location.watchPositionAsync({ accuracy: Location.Accuracy.Highest }, (newLocation) => {
+      const { latitude, longitude } = newLocation.coords
       setLocation({ latitude, longitude })
-    }
+    })
   }
 
-  console.log(`State: ${JSON.stringify(location)} / ${Object.keys(location).length} | ${JSON.stringify(geocode)} | ${errorMessage}.`)
+  const watchHeading = async () => {
+    Location.watchHeadingAsync(heading => setHeading(Math.round(heading.trueHeading)))
+  }
+
+  useEffect(() => {
+    getPermissions()
+  }, [])
+
+  useEffect(() => {
+    watchHeading()
+    watchPosition()
+  }, [permissionGranted])
+
+  const getDirectionLabel = () => {
+    const points = [
+      'North','NNE','NE','ENE',
+      'East', 'ESE','SE','SSE',
+      'South','SSW','SW','WSW',
+      'West', 'WNW','NW','NNW',
+      'North',
+    ]
+    return points[Math.round(heading / 22.5)]
+  }
+  const rotation = heading >= 45 ? heading : heading + 360
+  const compassRotateStyle = {
+    transform: [{ rotate: `${rotation - 45}deg`}],
+  }
+
+  console.log(`State: ${JSON.stringify(location)} / ${Object.keys(location).length} | ${JSON.stringify(heading)} | ${errorMessage}.`)
 
   return (
     <View style={styles.container}>
-      <Image source={ require("./assets/marker.png") } style={{ width: 100, height: 100 }} />
+      <Image source={require('./assets/compass.png')} style={[styles.compass, compassRotateStyle]} />
 
       {errorMessage.length > 0 &&
         <Text style={styles.heading2}>{errorMessage}</Text>
       }
 
-      {Object.keys(geocode).length > 0 &&
-        <>
-          <Text style={styles.heading1}>{geocode  ? `${geocode.city}, ${geocode.isoCountryCode}` : ""}</Text>
-          <Text style={styles.heading2}>{geocode ? geocode.street :""}</Text>
-        </>
-      }
+      <Text style={styles.heading2}>
+        {getDirectionLabel()}{' '}
+
+        <Text style={styles.heading3}>({heading}&deg;)</Text>
+      </Text>
 
       {Object.keys(location).length > 0 &&
         <Text style={styles.heading3}>{`${location.latitude}, ${location.longitude}`}</Text>
@@ -108,31 +98,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     alignItems: 'center',
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     flex: 1,
     justifyContent: 'center',
   },
-  overlay:{
+  overlay: {
     alignItems: "center",
-    backgroundColor: "#00000070",
+    backgroundColor: "#ffffff70",
     height: "100%",
     justifyContent: "center",
     width: "100%",
   },
-  heading1:{
-    color: "#fff",
-    fontSize: 30,
+  compass: {
+    height: 200,
+    marginBottom: 32,
+    width: 200,
+  },
+  heading1: {
+    color: "#777",
+    fontSize: 32,
     fontWeight: "bold",
     margin: 20
   },
-  heading2:{
-    color: "#fff",
-    fontSize: 15,
+  heading2: {
+    color: "#777",
+    fontSize: 18,
     fontWeight: "bold",
     margin: 5,
   },
-  heading3:{
-    color: "#fff",
+  heading3: {
+    color: "#777",
+    fontWeight: 'normal',
     margin: 5,
   }
 })
